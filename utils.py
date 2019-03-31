f8737f242759ad3633faac86197ac93f7520eb24
Ehsan <ehsan@debian>
2019-04-01 01:52:46 +0430

    added url_detector util function

---
+++ utils.py
@@ -0,0 +1,7 @@
+import re
+
+
+def url_detector(string):
+    """Find URLs in an string."""
+    regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    return regex.findall(string)
